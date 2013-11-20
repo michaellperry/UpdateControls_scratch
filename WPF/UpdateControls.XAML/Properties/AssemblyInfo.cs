fe19888afe49cdab2e4dc526a5958f0853cadf3a
Michael L Perry <michael@qedcode.com>
2013-11-20 14:39:53 -0800

    Silverlight

--- WPF/UpdateControls.XAML/Properties/AssemblyInfo.cs
+++ WPF/UpdateControls.XAML/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UpdateControls")]
-[assembly: AssemblyProduct("UpdateControls")]
+[assembly: AssemblyTitle("UpdateControls.XAML")]
+[assembly: AssemblyProduct("UpdateControls.XAML")]
 [assembly: ComVisible(false)]
